4a6d494d0c6711a6ee28d4b8e8facc87078a5027
Franco Zalamena <francozalamena@gmail.com>
2024-02-29 22:52:09 +0000

    Added default value for markerRes as it is not used yet

--- domain/src/main/java/com/piriurna/domain/models/PointOfInterestData.kt
+++ domain/src/main/java/com/piriurna/domain/models/PointOfInterestData.kt
@@ -4,6 +4,6 @@ data class PointOfInterestData(
     val id: Int = 0,
     val latitude: Double,
     val longitude: Double,
-    val markerRes: Int,
+    val markerRes: Int = 0,
     val name: String,
 )
