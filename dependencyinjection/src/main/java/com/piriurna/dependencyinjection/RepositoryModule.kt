9e6da46a14ce1811124db18be81ea574db3baded
Franco Zalamena <francozalamena@gmail.com>
2024-02-29 22:53:09 +0000

    Dependency Injection

--- dependencyinjection/src/main/java/com/piriurna/dependencyinjection/RepositoryModule.kt
+++ dependencyinjection/src/main/java/com/piriurna/dependencyinjection/RepositoryModule.kt
@@ -1,7 +1,10 @@
 package com.piriurna.dependencyinjection
 
 import com.piriurna.data.database.dao.PointOfInterestDao
+import com.piriurna.data.remote.sources.GeocodingApiSource
+import com.piriurna.data.repositories.GeocodingRepositoryImpl
 import com.piriurna.data.repositories.PointOfInterestRepositoryImpl
+import com.piriurna.domain.repositories.GeocodingRepository
 import com.piriurna.domain.repositories.PointOfInterestRepository
 import dagger.Module
 import dagger.Provides
@@ -17,5 +20,12 @@ object RepositoryModule {
     @Singleton
     fun providePointOfInterestRepository(pointOfInterestDao: PointOfInterestDao): PointOfInterestRepository {
         return PointOfInterestRepositoryImpl(pointOfInterestDao)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideGeocodingRepository(geocodingApiSource: GeocodingApiSource): GeocodingRepository {
+        return GeocodingRepositoryImpl(geocodingApiSource)
     }
 }
